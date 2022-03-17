fbbd2f1b9913b9e8b08d095f34aaeaa88d1249d0
Sara Gochez <saragochez13@gmail.com>
2022-03-17 00:18:36 -0700

    added the initial menu options

--- server.js
+++ server.js
@@ -1,26 +1,50 @@
-require('dotenv').config();
+//require('dotenv').config();
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+//const cTable = require('console.table');
 
-const dbConfig = {
+const db = mysql.createConnection({
   host: 'localhost',
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
-};
+});
+// db.query
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+function promptQuestions() {
+  console.log(`
+  *******************************************
+      WELCOME TO YOUR EMPLOYEE TRACKER
+  *******************************************
+  
+  `);
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'initOptions',
+        message: 'What would you like to do first?',
+        choices: [
+          'View All Employees',
+          'View All Employees By Department **',
+          'View All Employees by Manager **',
+          'Add Employee',
+          'Remove Employee **',
+          'Update Employee Role',
+          'Update Employee Manager **',
+        ],
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
+
+promptQuestions();
