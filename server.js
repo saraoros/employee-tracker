4e4b5e797fc428ad817161c5d792eb6eb035855b
Sara Gochez <saragochez13@gmail.com>
2022-03-19 15:51:27 -0700

    added sql query to all employees and all department functions

--- server.js
+++ server.js
@@ -83,7 +83,11 @@ function promptQuestions() {
 
 // View All Employees function
 const allEmployees = () => {
-  let sql = `SELECT * FROM employee`;
+  let sql = `SELECT employee.id, employee.first_name,
+             employee.last_name, department.department_name AS Department, role.salary
+             FROM employee, role, department
+             WHERE department.id = role.department_id
+             AND role.id = employee.role_id`;
 
   connection.query(sql, (err, res) => {
     if (err) throw err;
@@ -103,7 +107,9 @@ const allEmployees = () => {
 
 // View All Departments function
 const allDepartments = () => {
-  let sql = `SELECT * FROM department`;
+  let sql = `SELECT department.id, department.department_name 
+             AS Departments
+             FROM department`;
 
   connection.query(sql, (err, res) => {
     if (err) throw err;
@@ -144,7 +150,7 @@ const allRoles = () => {
 // View Add Employee function
 const addEmployee = () => {
   let sql = ``;
-
+  // need to add inquirer questions
   connection.query(sql, (err, res) => {
     if (err) throw err;
     console.table(res);
@@ -164,7 +170,7 @@ const addEmployee = () => {
 // Update an Employee function
 const updateEmployee = () => {
   let sql = ``;
-
+  // need to add inquirer questions here & add an array with employee name
   connection.query(sql, (err, res) => {
     if (err) throw err;
     console.table(res);
