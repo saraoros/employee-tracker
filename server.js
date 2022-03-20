117bc9b8fc37953b472b07ea1d4b04bf5f8f9b38
Sara Gochez <saragochez13@gmail.com>
2022-03-19 22:23:14 -0700

    starting update function over again

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function promptQuestions() {
       }
     })
     .catch((error) => {
-      if (error) throw error(404);
+      if (error) throw error();
     });
 }
 
@@ -84,7 +84,7 @@ const allEmployees = () => {
              AND role.id = employee.role_id`;
 
   connection.query(sql, (err, res) => {
-    if (err) throw err;
+    if (err) throw error;
     console.table(res);
     promptQuestions(
       console.log(`
@@ -106,7 +106,7 @@ const allDepartments = () => {
              FROM department`;
 
   connection.query(sql, (err, res) => {
-    if (err) throw err;
+    if (err) throw err0r;
     console.table(res);
     promptQuestions(
       console.log(`
@@ -182,7 +182,7 @@ const addEmployee = () => {
                      FROM role`;
 
       connection.query(sqlRole, (err, data) => {
-        if (err) throw err;
+        if (err) throw error;
 
         const rolesData = data.map(({ id, title }) => ({
           name: title,
@@ -204,7 +204,7 @@ const addEmployee = () => {
             // Capture new employee's manager
             const sqlManager = `SELECT * FROM employee`;
             connection.query(sqlManager, (err, data) => {
-              if (err) throw err;
+              if (err) throw error;
               const newManager = data.map(({ id, first_name, last_name }) => ({
                 name: first_name + ' ' + last_name,
                 value: id,
@@ -226,7 +226,7 @@ const addEmployee = () => {
                   const addEmployeeSql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
                                           VALUES (?, ?, ?, ?)`;
                   connection.query(addEmployeeSql, employeeInfo, (err) => {
-                    if (err) throw err;
+                    if (err) throw error;
                     console.log(`
            *******************************************
                       NEW EMPLOYEE ADDED
@@ -246,22 +246,62 @@ const addEmployee = () => {
 
 // Update an Employee function
 const updateEmployee = () => {
-  let sql = ``;
-  // need to add inquirer questions here & add an array with employee name
-  connection.query(sql, (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    promptQuestions(
-      console.log(`
+  let employeesNames = [];
+  let roles = [];
+
+  
+
+  connection.query(sql, (error, res) => {
+    if (error) throw error;
+
+    
+   
+   
+    
+    let sql = `SELECT role.id, role.title FROM role`;
+    connection.query(sql, (err, res) => {
+      if (err) throw err;
+      
+
+      res.forEach((role) => {
+        roles.push(role.title);
+      });
+
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'newEmployRole',
+            message: 'Whose role would you like to update?',
+            choices: employeesNames,
+          },
+          {
+            type: 'list',
+            name: 'employeeRoles',
+            message: `What is this employee's new role?`,
+            choices: roles,
+          },
+        ])
+        
+        .then((answer) => {
+      
+          });
+
+console.log("test")
+          let updatedEmploySql = `UPDATE employee SET employee.role_id = ? WHERE employee.id = ?`;
+
+        
+    });
+
+    console.log(`
     *******************************************
           EMPLOYEE HAS BEEN UPDATED
     *******************************************
 
+    `);
     
-    
-    `)
-    );
   });
+  promptQuestions();
 };
 
 promptQuestions();
